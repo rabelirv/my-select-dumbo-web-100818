d1916cb3cf69116beec4ff0ebd77149ee37c18e5
Irving E Rabel <rabel606@gmail.com>
2018-10-02 23:38:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ i = 0 
+ evens = []
+ while i < collection.length
+ evens<< if yield(collection[i],evens)
+ i= i + 1
+ end
+ end
+ evens
 end
+my_select([1,2,3,4,5]){|item| 
+item%2==0}
+  
